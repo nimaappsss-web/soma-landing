e6d525b307a65c84732fe18b153d2120432d8c61
Emmanuel Akinsiku <nimaappsss@gmail.com>
2026-09-21 12:31:35 +0100

    fix: apply h-screen-adaptive to hero for short viewports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <>
       <ScrollTracker />
-      <div id="home" className="h-screen min-h-[700px] p-[10px]">
+      <div id="home" className="h-screen h-screen-adaptive min-h-[700px] p-[10px]">
         <div className="relative h-full min-h-[680px] bg-soma-black rounded-[30px] overflow-hidden flex flex-col">
           <HeroBackground />
           <Navbar />
